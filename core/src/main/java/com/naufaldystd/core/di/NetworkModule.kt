09513f1c80b7e776e033cd6da6fb6372272aab43
blitzkz23 <blitzaldykazu@gmail.com>
2022-05-03 05:36:35 +0700

    Implemented SSL pinning on network module

--- core/src/main/java/com/naufaldystd/core/di/NetworkModule.kt
+++ core/src/main/java/com/naufaldystd/core/di/NetworkModule.kt
@@ -2,10 +2,16 @@ package com.naufaldystd.core.di
 
 import com.naufaldystd.core.data.source.remote.network.ApiService
 import com.naufaldystd.core.utils.Constants.BASE_URL
+import com.naufaldystd.core.utils.Constants.HOSTNAME
+import com.naufaldystd.core.utils.Constants.PIN_A
+import com.naufaldystd.core.utils.Constants.PIN_B
+import com.naufaldystd.core.utils.Constants.PIN_C
+import com.naufaldystd.core.utils.Constants.PIN_D
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -18,10 +24,21 @@ class NetworkModule {
 
 	@Provides
 	fun provideOkHttpClient(): OkHttpClient {
+		val certificatePinner = CertificatePinner.Builder()
+			.add(
+				HOSTNAME,
+				PIN_A,
+				PIN_B,
+				PIN_C,
+				PIN_D
+			)
+			.build()
+
 		return OkHttpClient.Builder()
 			.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
 			.connectTimeout(120, TimeUnit.SECONDS)
 			.readTimeout(120, TimeUnit.SECONDS)
+			.certificatePinner(certificatePinner)
 			.build()
 	}
 
--- core/src/main/java/com/naufaldystd/core/utils/Constants.kt
+++ core/src/main/java/com/naufaldystd/core/utils/Constants.kt
@@ -1,6 +1,8 @@
 package com.naufaldystd.core.utils
 
 import com.naufaldystd.core.BuildConfig
+import com.naufaldystd.core.utils.Constants.HOSTNAME
+import com.naufaldystd.core.utils.Constants.PIN_A
 
 object Constants {
 	const val BASE_URL = BuildConfig.RAWG_BASE_URL
@@ -8,5 +10,10 @@ object Constants {
 	const val DB_NAME = BuildConfig.DB_NAME
 	const val EXTRA_DATA = "extra_data"
 	const val PASSPHRASE = BuildConfig.PASSPHRASE
+	const val HOSTNAME = BuildConfig.HOSTNAME
+	const val PIN_A = BuildConfig.PIN_A
+	const val PIN_B = BuildConfig.PIN_B
+	const val PIN_C = BuildConfig.PIN_C
+	const val PIN_D = BuildConfig.PIN_D
 
 }
